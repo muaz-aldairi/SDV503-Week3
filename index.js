ad91283b1b200c6e2e75e5264e0f556f5b3c13e1
Muaz Aldairi <muaz-aldairi@live.nmit.ac.nz>
2020-03-10 11:36:23 +1300

    the end of the second class

--- index.js
+++ index.js
@@ -128,6 +128,20 @@ console.log( 1==="1");
 console.log(undefined > 0);
 console.log(undefined + 0);
 
+console.log(true||true);
+console.log(true||false);
+console.log(false||true);
+console.log(false||false);
+
+console.log(1||1);
+console.log(1||0);
+console.log(0||1);
+console.log(0||0);
+
+console.log(false&&true||false);
+
+
+
 
 
 
